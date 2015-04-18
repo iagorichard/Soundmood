86686bb5aed4b376e3c9c820547a7876da183b6f
Bianca Moreira <biancaazevmoreira@gmail.com>
2015-04-18 17:42:22 -0300

    Merge origin/master
    Conflicts:
    	SoundMood/src/controle/Cadastro.java
    	SoundMood/src/view/TelaSignUp.java

--- SoundMood/src/modelo/FreeUser.java
+++ SoundMood/src/modelo/FreeUser.java
@@ -5,20 +5,143 @@ package modelo;
  * @version 0.0
  */
 
-public class FreeUser extends User {
+public final class FreeUser extends User {
     
     /**Construtor para a classe FreeUser.
      * @author IagoRichard
      */
-    public FreeUser(long id, String name, String userName, String password, String email, String passwordTip){
+    
+    private long id;
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+    private String passwordTip;
+    
+    public FreeUser(long id, String name, String username, String password, String email, String passwordTip){
        
         setId(id);
         setName(name);
-        setUserName(userName);
+        setUserName(username);
         setPassword(password);
         setEmail(email);
         setPasswordTip(passwordTip);
         
     }
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+        //aqui coloca no banco de dados 
+        //INSERT INTO DATABASE ID
+    }
+
+    /**
+     * @param id the id to set
+     */
+    @Override
+    public void setId(long id) {
+        this.id = id;
+        //aqui coloca no banco de dados 
+        //INSERT INTO DATABASE ID
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+        //aqui coloca no banco de dados 
+        //INSERT INTO DATABASE ID
+    }
+
+    /**
+     * @param name the name to set
+     */
+    @Override
+    public void setName(String name) {
+        this.name = name;
+        //aqui coloca no banco de dados 
+        //INSERT INTO DATABASE ID
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+        //aqui coloca no banco de dados 
+        //INSERT INTO DATABASE ID
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+        //aqui coloca no banco de dados 
+        //INSERT INTO DATABASE ID
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+        //aqui coloca no banco de dados 
+        //INSERT INTO DATABASE ID
+    }
+
+    /**
+     * @param password the password to set
+     */
+    @Override
+    public void setPassword(String password) {
+        this.password = password;
+        //aqui coloca no banco de dados 
+        //INSERT INTO DATABASE ID
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+        //aqui coloca no banco de dados 
+        //INSERT INTO DATABASE ID
+    }
+
+    /**
+     * @param email the email to set
+     */
+    @Override
+    public void setEmail(String email) {
+        this.email = email;
+        //aqui coloca no banco de dados 
+        //INSERT INTO DATABASE ID
+    }
+
+    /**
+     * @return the passwordTip
+     */
+    public String getPasswordTip() {
+        return passwordTip;
+        //aqui coloca no banco de dados 
+        //INSERT INTO DATABASE ID
+    }
+
+    /**
+     * @param passwordTip the passwordTip to set
+     */
+    @Override
+    public void setPasswordTip(String passwordTip) {
+        this.passwordTip = passwordTip;
+        //aqui coloca no banco de dados 
+        //INSERT INTO DATABASE ID
+    }
+    
+    
     
 }
