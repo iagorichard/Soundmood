4de6559238f761baa2841be12ab487f074d17be2
Iago Silva <iagorrs@gmail.com>
2015-05-27 20:59:37 -0300

    Documentação JavaDoc

--- SoundMood/src/controller/ControleCadastro.java
+++ SoundMood/src/controller/ControleCadastro.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
 import java.util.List;
@@ -15,6 +10,9 @@ import org.hibernate.Transaction;
 /**
  *
  * @author biancamoreira
+ * @author Iago Rodrigues
+ * @author Leylane Ferreira
+ * @author Washington Filipe
  */
 public class ControleCadastro {
     
