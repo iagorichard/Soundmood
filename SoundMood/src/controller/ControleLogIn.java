2bd2bbd418966fe02470e33fbc6a4cc4d91774b0
Iago Silva <iagorrs@gmail.com>
2015-05-27 22:18:57 -0300

    Documentação ControleLogin

--- SoundMood/src/controller/ControleLogIn.java
+++ SoundMood/src/controller/ControleLogIn.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
 import java.util.List;
@@ -12,30 +7,52 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 
 /**
+ * Classe responsável pelo controle de <b>login</b> da seção atual de um
+ * determinado usuário.
  *
+ * @version 1.0
  * @author biancamoreira
+ * @author Iago Rodrigues
+ * @author Leylane Ferreira
+ * @author Washington Filipe
  */
 public class ControleLogIn {
     
-    public boolean checaLogin(String username, String password){
-        ConnectionBD connect = new ConnectionBD();
-        Session session = connect.abrirConexao();
+    /** Construtor simples para a classe. */
+    public ControleLogIn(){
         
-        session.beginTransaction();
-        Query query = session.createQuery("from Usuario where username= :username");
-        query.setParameter("username",username);
-        
-        List<Usuario> users = query.list();
-       
-        
-        for(Usuario user : users){
-            if(password.equals(user.getSenha())){
-                return true;
-            }
-           
-        }
-        
-        return false;
     }
     
+    /**
+     * Método responsável para a validação de um login, o usuário só poderá
+     * logar no sistema se o username e password corresponderem a um mesmo
+     * usuário e senha que estiverem cadastrados na mesma tupla do banco
+     * de dados do sistema.
+     * @param username Nome de usuário a ser validado.
+     * @param password Senha a ser validada.
+     * @return boolean: se o username e password tiverem sido validados, significa
+     * que o login foi efetuado com sucesso, fazendo com que o método retorne
+     * a <b>true</b>, se não retornará a <b>false</b>.
+     */
+    public boolean checaLogin(String username, String password) {
+        
+        ConnectionBD connect = new ConnectionBD();
+        Session session = connect.abrirConexao();
+
+        session.beginTransaction();
+        Query query = session.createQuery("from Usuario where username= :username");
+        query.setParameter("username", username);
+
+        List<Usuario> users = query.list();
+
+        for (Usuario user : users) {
+            if (password.equals(user.getSenha())) {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
 }
