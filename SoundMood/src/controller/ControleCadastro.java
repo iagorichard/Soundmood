fc8c725ed1caaec036c0de221152e9e800e4efd0
Iago Silva <iagorrs@gmail.com>
2015-05-27 21:16:58 -0300

    Documentação JavaDoc2

--- SoundMood/src/controller/ControleCadastro.java
+++ SoundMood/src/controller/ControleCadastro.java
@@ -8,119 +8,123 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 /**
- * Classe responsável pelo controle de cadastro de um novo usuário do sistema, 
- * aqui estarão todos os métodos para a validação desse novo usuário.
+ * Classe responsável pelo controle de cadastro de um novo <b>usuário</b> do
+ * sistema, aqui estarão todos os métodos para a validação desse novo usuário.
+ *
  * @version 1.0
  * @author biancamoreira
  * @author Iago Rodrigues
  * @author Leylane Ferreira
  * @author Washington Filipe
  */
-
 public class ControleCadastro {
 
-    
-    public String receberInformacao(String username, String email){
-         try{
-                //Verificar o usuario -- verificar se existe
-                if(this.verificarUsuario(username)==true){
-                    /** Se retornar a true, quer dizer que o usuário com esse username já existe
-                     * o método retorna um valor para ser mostrada uma mensagem
-                     */
-                    return "usuario";
-                }else if(this.verificarEmail(email)==true){    
-                    /** Se retornar a true, quer dizer que o usuário com esse username já existe
-                     * o método retorna um valor para ser mostrada uma mensagem
-                     */
-                    return "email";
-                }else{
-                    return "disponivel";
-                }
-         }catch(Exception e){
-         }
-         return "disponivel";
+    /**
+     * Método que recebe as informações do novo usuário a ser cadastrado, e
+     * verifica se realmente poderá ser criado este usuário com as informações
+     * passadas como parâmetro; será realizada uma busca no banco de dados
+     * para saber se existe um usuário com o username e email que foi passado
+     * como parâmetro.
+     * @param username - username do suposto novo usuário.
+     * @param email - e-mail do suposto novo usuário.
+     * @return - String, se o cadastro for aprovado, retorna uma mensagem <b>"Disponivel"</b>, se não, retorna outro valor. 
+     */
+    public String receberInformacao(String username, String email) {
+        try {
+            //Verificar o usuario -- verificar se existe
+            if (this.verificarUsuario(username) == true) {
+                /**
+                 * Se retornar a true, quer dizer que o usuário com esse
+                 * username já existe o método retorna um valor para ser
+                 * mostrada uma mensagem
+                 */
+                return "usuario";
+            } else if (this.verificarEmail(email) == true) {
+                /**
+                 * Se retornar a true, quer dizer que o usuário com esse
+                 * username já existe o método retorna um valor para ser
+                 * mostrada uma mensagem
+                 */
+                return "email";
+            } else {
+                return "disponivel";
+            }
+        } catch (Exception e) {
+        }
+        return "disponivel";
     }
-    
-    public boolean gravarInformacao(String username, String password, String nomeCompleto, String email, int tipoConta, String passwordTip){
-        
-        try{
-        ConnectionBD connect = new ConnectionBD();
-        Session session = connect.abrirConexao();
-        
-        
-        
-        Usuario user = new Usuario();
-        
-        user.setUsername(username);
-        //user.setTipo(tipoConta);
-        user.setNome(nomeCompleto);
-        user.setSenha(password);
-        user.setSenhadica(passwordTip);
-        user.setEmail(email);
-       
-        
-        System.out.println(nomeCompleto);
-        session.beginTransaction();
-        
-        //Save the employee in database
-        session.saveOrUpdate(user);
-        
-        session.getTransaction().commit();
-        
- 
+
+    public boolean gravarInformacao(String username, String password, String nomeCompleto, String email, int tipoConta, String passwordTip) {
+
+        try {
+            ConnectionBD connect = new ConnectionBD();
+            Session session = connect.abrirConexao();
+
+            Usuario user = new Usuario();
+
+            user.setUsername(username);
+            //user.setTipo(tipoConta);
+            user.setNome(nomeCompleto);
+            user.setSenha(password);
+            user.setSenhadica(passwordTip);
+            user.setEmail(email);
+
+            System.out.println(nomeCompleto);
+            session.beginTransaction();
+
+            //Save the employee in database
+            session.saveOrUpdate(user);
+
+            session.getTransaction().commit();
+
         //Commit the transaction
-        //session.getTransaction().commit();
-        
+            //session.getTransaction().commit();
         //session.close();
-        
-        return true;
-        
-        }catch(Exception e){
+            return true;
+
+        } catch (Exception e) {
             return false;
         }
-        
+
     }
-    
-    
+
     private boolean verificarUsuario(String username) {
         ConnectionBD connect = new ConnectionBD();
         Session session = connect.abrirConexao();
-        
+
         session.beginTransaction();
         Query query = session.createQuery("from Usuario");
-        
+
         List<Usuario> users = query.list();
-       
-        
-        for(Usuario user : users){
-            
-            if(username.equals(user.getUsername())){
+
+        for (Usuario user : users) {
+
+            if (username.equals(user.getUsername())) {
                 return true;
             }
         }
-        
+
         return false;
-        
+
     }
-    
+
     private boolean verificarEmail(String email) {
         ConnectionBD connect = new ConnectionBD();
         Session session = connect.abrirConexao();
-        
+
         Transaction tx = session.beginTransaction();
         Query query = session.createQuery("from Usuario");
-        
+
         List<Usuario> users = query.list();
-       
-        
-        for(Usuario user : users){
-            
-            if(email.equals(user.getEmail())){
+
+        for (Usuario user : users) {
+
+            if (email.equals(user.getEmail())) {
                 return true;
             }
         }
-        
+
         return false;
-        
+
     }
 }
