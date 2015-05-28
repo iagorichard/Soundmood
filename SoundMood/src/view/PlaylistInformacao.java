d6bc6912df3b8153f62bdc156c35a751af22f736
Iago Silva <iagorrs@gmail.com>
2015-05-28 20:12:02 -0300

    PlaylistInformacao

--- SoundMood/src/view/PlaylistInformacao.java
+++ SoundMood/src/view/PlaylistInformacao.java
@@ -14,47 +14,45 @@ import javax.swing.JLabel;
  * @author Washington Filipe
  */
 public class PlaylistInformacao extends Thread {
-    
+
     private JFrame MP3Gui;
     private JLabel jlabel1, jlabel2;
     ControlePlaylist CP = ControlePlaylist.getInstance();
-    
+
     String nomeMusica;
     String nomeArtista;
-    
+
     /**
-     * Método para setar as informações da playlist.
-     * @param MP3Gui
-     * @param jlabel1
-     * @param jlabel2 
+     * Construtor que seta as informações da playlist.
+     * @param MP3Gui Frame da interface gráfica da playlist.
+     * @param jlabel1 Texto1 da playlist.
+     * @param jlabel2 Texto2 da playlist.
      */
-    public PlaylistInformacao(JFrame MP3Gui, JLabel jlabel1, JLabel jlabel2){
-        
+    public PlaylistInformacao(JFrame MP3Gui, JLabel jlabel1, JLabel jlabel2) {
+
         this.MP3Gui = MP3Gui;
         this.jlabel1 = jlabel1;
         this.jlabel2 = jlabel2;
-        
+
     }
-    
+
     /**
-     * Rodando e atualizando as informações da playlist de acordo com a música 
+     * Rodando e atualizando as informações da playlist de acordo com a música
      * atual através de uma thread.
      */
     @Override
-    public void run(){
-        
-         while(true){ 
-             
-             //System.out.println("funfando");
-           nomeMusica = CP.getMusicaNome();
-           nomeArtista = CP.getMusicaArtista();
-            
-           jlabel1.setText(nomeMusica);
-           jlabel2.setText(nomeArtista);
-         }
-        
+    public void run() {
+
+        while (true) {
+
+            //System.out.println("funfando");
+            nomeMusica = CP.getMusicaNome();
+            nomeArtista = CP.getMusicaArtista();
+
+            jlabel1.setText(nomeMusica);
+            jlabel2.setText(nomeArtista);
+        }
+
     }
-    
-    
-    
+
 }
